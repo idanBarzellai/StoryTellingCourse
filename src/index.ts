eb6e846caa012bc5d701fdd1c9c874885c2d718f
idanBarzellai <idanbarzi@gmail.com>
2025-06-15 18:30:32 +0300

    removed unrealted

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import Level from "./scenes/Level";
 import preloadAssetPackUrl from "../static/assets/preload-asset-pack.json";
 import Preload from "./scenes/Preload";
 import VisualNovel from "./scenes/VisualNovel";
@@ -31,7 +30,7 @@ window.addEventListener('load', function () {
 			mode: Phaser.Scale.ScaleModes.FIT,
 			autoCenter: Phaser.Scale.Center.CENTER_BOTH
 		},
-		scene: [Boot, Preload, Level, VisualNovel]
+		scene: [Boot, Preload, VisualNovel]
 	});
 
 	game.scene.start("Boot");
